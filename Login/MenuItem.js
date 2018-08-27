51412dec6b5358b178bceef61f62e3c63aad6812
Adeismail <42646604+Adeismail@users.noreply.github.com>
2018-08-27 17:04:14 +0700

    pp

---
+++ Login/MenuItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    View,
+    Image,
+    StyleSheet,
+} from 'react-native';
+
+
+export default class MenuItem extends React.Component{
+    render(){
+        return(
+            <View style={styles.menuItem}>
+                <Image
+                    source={this.props.itemImage}
+                    style={styles.image}/>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    menuItem: {
+        width: '33.333333%',
+        height: '50%',
+        padding: 20,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        opacity: 0.8,
+        borderColor:'#fff',
+        borderWidth: '3',
+    },
+})
